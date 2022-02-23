fd2c508e91fc2b2f9f5bce2d2a059f56655f5ec2
James McNeil <mcneil_james@hotmail.com>
2022-02-23 21:33:01 +0000

    created express app

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Server...')
+// import express
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+// initaial app as express
+const app = express()
+
+// add route
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+// set app to listen on port
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`)
+})
