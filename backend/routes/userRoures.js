2f2d031862b5c2247354de7f51139d19e836974e
James McNeil <mcneil_james@hotmail.com>
2022-02-24 18:48:54 +0000

    added toutes for / and /login

--- backend/routes/userRoures.js
+++ backend/routes/userRoures.js
@@ -1,12 +1,8 @@
 const express = require('express')
-
 const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/userController')
 
-router.post('/', (req, res) => {
-  res.send('Register Route')
-})
-router.post('/login', (req, res) => {
-  res.send('Login Route')
-})
+router.post('/', (req, res) => registerUser)
+router.post('/login', (req, res) => loginUser)
 
 module.exports = router
