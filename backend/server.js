d9008f1d4c3961c8f11ee4e58fcea897de170412
James McNeil <mcneil_james@hotmail.com>
2022-02-24 19:42:58 +0000

    express bodyparser with urlencoded & imported errorHandler

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,19 @@ const PORT = process.env.PORT || 5000
 // initaial app as express
 const app = express()
 
+// middlewares
+const { errorHandler } = require('./middleware/errorMiddleware')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // add route
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' })
 })
 
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 // set app to listen on port
 app.listen(PORT, () => {
