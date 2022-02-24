4c2efcc33e4a2227dd218e3bec881b3568059581
James McNeil <mcneil_james@hotmail.com>
2022-02-24 19:50:52 +0000

    import asyncHandler and wrap functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
   // validation
@@ -10,14 +12,13 @@ const registerUser = (req, res) => {
     throw new Error('Please include all fields!')
   }
   res.send('Register Route')
-}
-
+})
 // @desc    login a user
 // @route   /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route')
-}
+})
 
 module.exports = {
   registerUser,
