6b46bbd1ebc12356ebaa2476d4b8f47c1a17b401
James McNeil <mcneil_james@hotmail.com>
2022-03-01 17:21:56 +0000

    creating NewTicket form

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const NewTicket = () => {
-  return <div>NewTicket</div>
+  const { user } = useSelector((state) => state.auth)
+  const name = user.name
+  const email = user.email
+  const [product, setProduct] = useState('iPhone')
+  const [description, setDescription] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <>
+      <section className='heading'>
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+      <section className='form'>
+        <div className='form-group'>
+          <label htmlFor='name'>Customer Name</label>
+          <input type='text' className='form-control' value={name} disabled />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Customer Email</label>
+          <input type='text' className='form-control' value={email} disabled />
+        </div>
+        <form onSubmit={onSubmit}>
+          <label htmlFor='product'>Product</label>
+          <input type='text' className='form-control' value={name} disabled />
+        </form>
+      </section>
+    </>
+  )
 }
 
 export default NewTicket
