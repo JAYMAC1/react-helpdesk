8fe90aa94d2f6cd0a625d81e594940bee6649555
James McNeil <mcneil_james@hotmail.com>
2022-03-02 20:01:12 +0000

    import TicketItem

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -10,6 +10,7 @@ import { getUserTickets, reset } from '../features/tickets/ticketSlice'
 // custom pages & components
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 const Tickets = () => {
   const { tickets, isError, isLoading, isSuccess } = useSelector(
@@ -32,7 +33,23 @@ const Tickets = () => {
 
   if (isLoading) <Spinner />
 
-  return <div>Tickets</div>
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Tickets
