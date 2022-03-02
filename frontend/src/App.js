9d1354fe367fa1f44217481e831e5c61a6d4a183
James McNeil <mcneil_james@hotmail.com>
2022-03-02 19:25:45 +0000

    imported Ticket page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
 import NewTicket from './pages/NewTicket'
+import Tickets from './pages/Tickets'
 import PrivateRoute from './components/PrivateRoute'
 
 // misc packages
@@ -25,6 +26,9 @@ function App() {
             <Route path='/register' element={<Register />} />
             <Route path='/new-ticket' element={<PrivateRoute />}>
               <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
+            <Route path='/tickets' element={<PrivateRoute />}>
+              <Route path='/tickets' element={<Tickets />} />
             </Route>
           </Routes>
         </div>
