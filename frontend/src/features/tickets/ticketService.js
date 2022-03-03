9e8a314546b222b5e43df60c6cca2cdc5db57271
James McNeil <mcneil_james@hotmail.com>
2022-03-03 07:21:23 +0000

    ctrated getTicket service

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -30,8 +30,20 @@ const getUserTickets = async (token) => {
   return response.data
 }
 
+// Get all user tickets
+const getTicket = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.get(`${API_URL}/${ticketId}`, config)
+  return response.data
+}
+
 const ticketService = {
   createTicket,
+  getTicket,
   getUserTickets,
 }
 
