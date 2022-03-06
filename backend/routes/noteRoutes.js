be538eccbd6376a495553a612c172e0bec83a3da
James McNeil <mcneil_james@hotmail.com>
2022-03-06 11:53:06 +0000

    initial creation of noteRouter

---
+++ backend/routes/noteRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const { getNotes } = require('../controllers/noteController')
+
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getNotes).post
+
+// router
+//   .route('/:id')
+//   .get(protect, getTicket)
+//   .delete(protect, deleteTicket)
+//   .put(protect, updateTicket)
+module.exports = router
