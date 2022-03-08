ec878d1564909da8ec650044be40057d963bb982
James McNeil <mcneil_james@hotmail.com>
2022-03-08 21:56:16 +0000

    swapped createNote to test passing of arguments

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -93,7 +93,7 @@ const Ticket = () => {
   const onFormSubmit = (e) => {
     e.preventDefault()
     console.log(noteText, id)
-    dispatch(createNote(noteText, id))
+    dispatch(createNote(id, noteText))
     closeModal()
     setNoteText('')
   }
